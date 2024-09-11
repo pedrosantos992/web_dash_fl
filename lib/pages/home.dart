3dbfc020c214d151c36935d8642810a7b274a03f
Pedro Santos <pedro.santos@pipecodes.com>
2024-09-11 14:44:08 +0100

    data search

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/user_model.dart'; 
-
+import '../models/user_model.dart';
 
 Future<List<UserModel>> fetchUsers() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
 
   if (response.statusCode == 200) {
     // Parse the JSON response
@@ -27,11 +27,44 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late Future<List<UserModel>> futureUsers;
+  List<UserModel> _users = [];
+  List<UserModel> _filteredUsers = [];
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     futureUsers = fetchUsers(); // Fetch data when the screen is loaded
+
+    futureUsers.then((users) {
+      setState(() {
+        _users = users;
+        _filteredUsers = users; // Initially show all posts
+      });
+    });
+    _searchController.addListener(_filterUsers);
+  }
+
+  @override
+  void dispose() {
+    _searchController
+        .dispose(); // Dispose the controller when the widget is removed
+    super.dispose();
+  }
+
+  void _filterUsers() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredUsers = _users.where((user) {
+        List<String> searchableFields = [
+          user.name.toLowerCase(),
+          user.username.toLowerCase(),
+          user.email.toLowerCase(),
+          user.company.name.toLowerCase(),
+        ];
+        return searchableFields.any((field) => field.contains(query));
+      }).toList();
+    });
   }
 
   @override
@@ -40,45 +73,68 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: appBar(context),
       drawer: isMobile ? drawer(context) : null,
-      body: FutureBuilder<List<UserModel>>(
-        future: futureUsers,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No data available'));
-          }
-
-          List<UserModel> users = snapshot.data!;
-
-          return SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: DataTable(
-              columns: const [
-                DataColumn(label: Text('ID')),
-                DataColumn(label: Text('Name')),
-                DataColumn(label: Text('Username')),
-                DataColumn(label: Text('Email')),
-              ],
-              rows: users
-                  .map(
-                    (user) => DataRow(
-                      cells: [
-                        DataCell(Text(user.id.toString())),
-                        DataCell(Text(user.name)),
-                        DataCell(Text(user.username)),
-                        DataCell(Text(user.email)),
-                      ],
-                    ),
-                  )
-                  .toList(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search by name or company",
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  )),
             ),
-          );
-        },
-      )
-      ,// This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          Expanded(
+            child: FutureBuilder<List<UserModel>>(
+              future: futureUsers,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No data available'));
+                }
+
+                return _filteredUsers.isNotEmpty
+                    ? SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: DataTable(
+                          columns: const [
+                            DataColumn(label: Text('ID')),
+                            DataColumn(label: Text('Name')),
+                            DataColumn(label: Text('Username')),
+                            DataColumn(label: Text('Email')),
+                            DataColumn(label: Text('Company')),
+                          ],
+                          rows: _filteredUsers
+                              .map(
+                                (user) => DataRow(
+                                  cells: [
+                                    DataCell(Text(user.id.toString())),
+                                    DataCell(Text(user.name)),
+                                    DataCell(Text(user.username)),
+                                    DataCell(Text(user.email)),
+                                    DataCell(Text(user.company.name)),
+                                  ],
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      )
+                    : const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text("No results found!"),
+                      );
+              },
+            ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+        ],
+      ),
     );
   }
 
@@ -87,24 +143,28 @@ class _MyHomePageState extends State<MyHomePage> {
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       title: Text(widget.title),
-      actions: !isMobile ? [
-          TextButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/about'); // Navigate to the About page
-          },
-          child: const Text(
-            'About',
-            style: TextStyle(color: Colors.white),
-          ),
-        ),
-      ] : null,
+      actions: !isMobile
+          ? [
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(
+                      context, '/about'); // Navigate to the About page
+                },
+                child: const Text(
+                  'About',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ]
+          : null,
     );
   }
 
   Theme drawer(BuildContext context) {
     return Theme(
       data: Theme.of(context).copyWith(
-        canvasColor: Colors.white, // Change this if you want a custom background color
+        canvasColor:
+            Colors.white, // Change this if you want a custom background color
       ),
       child: Drawer(
         shape: const RoundedRectangleBorder(
